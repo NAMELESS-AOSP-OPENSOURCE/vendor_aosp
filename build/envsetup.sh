2b13b77ddcff5a039916532291125ad4a0d0a8a4
Timi Rautamäki <timi.rautamaki@gmail.com>
2022-03-24 08:21:34 +0000

    vendor: move mka definition above brunch
    Otherwise it's not defined for brunch.
    
    Change-Id: I642e3db365fab46d4428d1b1f310304280b72efc

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -18,6 +18,8 @@ Additional functions:
 EOF
 }
 
+alias mka=m
+
 function mk_timer()
 {
     local start_time=$(date +"%s")
@@ -381,8 +383,6 @@ function __detect_shell() {
     esac
     return
 }
-
-alias mka=m
 
 function cmka() {
     if [ ! -z "$1" ]; then
