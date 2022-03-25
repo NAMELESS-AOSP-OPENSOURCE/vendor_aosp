999c1da6632554254aea4e54bfd93be938955209
Nolen Johnson <johnsonnolen@gmail.com>
2022-03-24 21:35:33 -0400

    vendor: Make mka a function again
    * This caused breakages in subshells from scripts.
    
    * Still keep the dropping of `-j`, as it's not needed.
    
    This is a squashed revert of:
    
        Author:     Timi Rautamaki <timi.rautamaki@gmail.com>
        AuthorDate: 2022-03-24 08:21:34 +0000
        lineage: move mka definition above brunch
    
        Otherwise it's not defined for brunch.
    
        Change-Id: I642e3db365fab46d4428d1b1f310304280b72efc
    
        Author:     Alexander Koskovich <akoskovich@pm.me>
        AuthorDate: 2022-03-21 07:54:20 -0700
    
        envsetup: Remove threads argument from mka
    
         * "m" is multi-threaded by default.
    
        Change-Id: I5c9bb30dc49dafa31ba9fc70e617f477b6814b41
    
    Change-Id: If4112ae739d8356f922cbbaf8e3d7ded5d078bd0

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -18,8 +18,6 @@ Additional functions:
 EOF
 }
 
-alias mka=m
-
 function mk_timer()
 {
     local start_time=$(date +"%s")
@@ -382,6 +380,10 @@ function __detect_shell() {
             ;;
     esac
     return
+}
+
+function mka() {
+    m
 }
 
 function cmka() {
