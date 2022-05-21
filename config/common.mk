03982cb368b1766a1099ccc79cf0314923a75905
cjh1249131356 <cjh1249131356@gmail.com>
2022-05-21 12:52:11 +0800

    vendor: permission: Clean up deprecated lineagehw permission
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -36,7 +36,6 @@ endif
 
 # Some permissions
 PRODUCT_COPY_FILES += \
-    vendor/aosp/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml \
     vendor/aosp/config/permissions/privapp-permissions-settings.xml::$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-settings.xml
 
 # Copy all custom init rc files
