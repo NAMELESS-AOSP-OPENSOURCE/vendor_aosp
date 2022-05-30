47b56951abb009cd195bd7cf9b219ac64b470037
cjh1249131356 <cjh1249131356@gmail.com>
2022-05-30 22:04:42 +0800

    vendor: Make bootanimation work well with lower resolution [2/2]
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,4 +1,3 @@
-
 # Bootanimation
 ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
      PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
@@ -13,4 +12,12 @@ else
         $(warning "Current bootanimation res is not supported, forcing 1080p")
     endif
     PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
+
+ifeq ($(TARGET_BOOT_ANIMATION_LOWER_RES),1080)
+    PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-lower_resolution.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_LOWER_RES),1440)
+    PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-lower_resolution.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_LOWER_RES),720)
+    PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-lower_resolution.zip
 endif
