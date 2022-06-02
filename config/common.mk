83714ab0b59feb091814a3b7cf9b9cc446ae7579
cjh1249131356 <cjh1249131356@gmail.com>
2022-06-02 16:57:23 +0800

    vendor: Switch to GameSpace
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -214,9 +214,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
-# GamingMode
+# Game space
 PRODUCT_PACKAGES += \
-    GamingMode
+    GameSpace
 
 # Launcher
 PRODUCT_PACKAGES += \
