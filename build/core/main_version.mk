a78fc4ecd719d0b9529cbf8a6753265aad0978af
Aryan Sinha <sinha.aryan03@gmail.com>
2022-07-07 11:44:36 +0000

    vendor: Show Android version as 12L [2/2] * Android 12 & 12L, Both Show Platform version as "12", while 12L is not a QPR release , its a Android Bump   ( Android 12 = VNDK31, Android 12.1/12L = VNDK 32 ), Even Google released the newer tags with android-12.1.0_r1 ,    not with android-12.0.0 branding [1], Some end users do not know if they're running Android 12 or 12L, this's because    Android Version Being displayed as "12" on both 12 & 12L, Even Google Docs [2] refer the newer Android Version as 12L,    I've no idea why they didnt care to bump it here, hence refactor PLATFORM_VERSION_LAST_STABLE to 12L
    [1] - https://android.googlesource.com/platform/manifest/+/refs/tags/android-12.1.0_r1
    [2] - https://developer.android.com/about/versions/12/12L/summary
    Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>
    Signed-off-by: anupam41 <anupamsamanta41@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I29d6afc9bbb105549651697b3ff2ab33995050ae
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,3 +1,6 @@
+# Versioning System
+CUSTOM_PLATFORM_RELEASE_OR_CODENAME := 12L
+
 # Build fingerprint
 ifeq ($(BUILD_FINGERPRINT),)
 BUILD_NUMBER_CUSTOM := $(shell date -u +%H%M)
@@ -18,4 +21,5 @@ ADDITIONAL_SYSTEM_PROPERTIES  += \
     ro.nameless.version.display=$(CUSTOM_VERSION) \
     ro.nameless.build_date=$(CUSTOM_BUILD_DATE) \
     ro.nameless.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
-    ro.nameless.build_type=$(CUSTOM_BUILD_TYPE)
+    ro.nameless.build_type=$(CUSTOM_BUILD_TYPE) \
+    ro.nameless.settings.android_version=$(CUSTOM_PLATFORM_RELEASE_OR_CODENAME)
